9417675cecd9a8a2bf0733604ba540e90d463d29
Rodrigo Haiache <rodrigohi_15@hotmail.com>
2020-04-05 17:31:21 -0300

    commit parte A

---
+++ Usuario.java
@@ -0,0 +1,10 @@
+class Usuario{
+
+    private String nombre;
+    private String cedula;
+    private String contraseña;
+    private int edad;
+    private int cantidadHijos;
+    private int saldo = 0;
+
+}
