08ed3ee177b5d7a182fab43da64973f73e43053a
Rodrigo Haiache <rodrigohi_15@hotmail.com>
2020-04-05 17:40:08 -0300

    commit Parte B

--- Usuario.java
+++ Usuario.java
@@ -7,4 +7,61 @@ class Usuario{
     private int cantidadHijos;
     private int saldo = 0;
 
+    public Usuario(){
+
+    }
+
+    public void setNombre(String nom){
+        this.nombre = nom;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+   
+    
+    public void setCedula(String ced){
+        this.cedula = ced;
+    }
+
+    public String getCedula(){
+        return this.cedula;
+    }
+   
+   
+    public void setContraseña(String contr){
+        this.contraseña = contr;
+    }
+
+    public String getContraseña(){
+        return this.contraseña;
+    }
+
+
+    public void setEdad(int ed){
+        this.edad = ed;
+    }
+
+    public int getEdad(){
+        return this.edad;
+    }
+   
+    public void setCantidadHijos(int cant){
+        this.cantidadHijos = cant;
+    }
+
+    public int getCantHijos(){
+        return this.cantidadHijos;
+    }
+    
+    
+    public void setSaldo(int sal){
+        this.saldo = sal;
+    }
+
+    public int getSaldo(){
+        return this.saldo;
+    }
+
+
 }
