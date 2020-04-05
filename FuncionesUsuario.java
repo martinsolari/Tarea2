68bd8687a485d30b1022f929cbd425b452f86b09
Rodrigo Haiache <rodrigohi_15@hotmail.com>
2020-04-05 19:34:49 -0300

    commit parte A Funciones

---
+++ FuncionesUsuario.java
@@ -0,0 +1,34 @@
+import Usuario.java;
+
+class FuncionesUsuario{
+
+    public int CalcularImpuestos(Usuario u){
+        
+        int edad = u.getEdad();
+        float imp=0;
+        
+        if (edad<18){
+            return imp;
+        }
+
+        else{
+        else if(edad>=18 && edad<=30){
+            imp = 2000;
+        }
+
+        else if(edad>30 && edad<65){
+            imp = 7000;
+        }
+
+        else if(edad>65){
+           imp = 2000;
+        }
+        
+         imp = imp - ((u.getCantHijos()*100)/3);
+        }
+
+        return imp;
+
+    }
+
+}
